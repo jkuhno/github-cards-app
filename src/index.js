3271a9988b9e378eb7580711f56649890bec2a07
Jani Kuhno <jani.kuhno@tuni.fi>
2021-02-09 14:11:00 +0200

    Finished

--- src/index.js
+++ src/index.js
@@ -1,17 +1,86 @@
+//Start with what makes sense right now. Rename/Delete later.
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+/*const testData = [
+    {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+    {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+    {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+];*/
+
+const axios = require('axios').default;
+
+const CardList = (props) => (
+    <div>
+        {props.profiles.map(profile => <Card key={profile.id} {...profile} />)}
+    </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Card extends React.Component {
+    render() {
+        const profile = this.props;
+        return (
+            <div className="github-profile">
+                <img src={profile.avatar_url} alt="GitHub profile avatar" />
+                <div className="info">
+                    <div className="name">{profile.name}</div>
+                    <div className="company">{profile.company}</div>
+                </div>
+            </div>
+        );
+    }
+}
+
+class Form extends React.Component {
+    state = { userName: '' };
+    handleSubmit = async (event) => {
+        event.preventDefault();
+        const resp = await axios.get(`https://api.github.com/users/${this.state.userName}`);
+        this.props.onSubmit(resp.data);
+        this.setState({ userName: '' });
+        // ADD ERROR HANDLING
+    };
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input
+                    type="text"
+                    value={this.state.userName}
+                    onChange={event => this.setState({ userName: event.target.value })}
+                    placeholder="GitHub username"
+                    required
+                />
+                <button>Add card</button>
+            </form>
+        );
+    }
+}
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            profiles: [],
+        };
+    }
+    addNewProfile = (profileData) => {
+        this.setState(prevState => ({
+            profiles: [...prevState.profiles, profileData],
+        }));
+    };
+    render() {
+        return (
+            <div>
+                <div className="header">{this.props.title}</div>
+                <Form onSubmit={this.addNewProfile} />
+                <CardList profiles={this.state.profiles} />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <App title="The GitHub Cards App" />,
+    document.getElementById("root"),
+);
